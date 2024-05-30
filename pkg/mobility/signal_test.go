a3ce09e94c705dad4d47d86b3ac2311fe1673f62
Doukas George <gdoukas@intracom-telecom.com>
2024-05-30 14:05:46 +0300

    added testpathloss to compare values

--- pkg/mobility/signal_test.go
+++ pkg/mobility/signal_test.go
@@ -5,6 +5,8 @@ import (
 	"math"
 	"testing"
 	"fmt"
+	"encoding/csv"
+	"os"
 )
 
 
@@ -402,5 +404,58 @@ func TestGetChangingPositionPathLossRuralNLOS(t *testing.T) {
 
 	if firstPathLoss > secondPathLoss {
 		t.Errorf("Expected %f but got %f", firstPathLoss, secondPathLoss)
+	}
+}
+
+
+func TestPathloss(t *testing.T) {
+	cell := model.Cell{
+		Sector: model.Sector{
+			Center: model.Coordinate{Lat: 0, Lng: 0},
+			Height: 35,
+		},
+		Channel: model.Channel{
+			SSBFrequency: 3000,
+		},
+	}
+
+	file, err := os.Create("pathloss.csv")
+	if err != nil {
+		t.Fatalf("failed to create file: %v", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	header := []string{"Index", "UE position", "distance3D", "pathlossUmaLOS", "pathlossUmaNLOS", "pathlossRmaLOS", "pathlossRmaNLOS"}
+	if err := writer.Write(header); err != nil {
+		t.Fatalf("failed to write header to CSV: %v", err)
+	}
+
+	fmt.Printf("Index \t| UE position \t\t| distance3D \t| pathlossUmaLOS \t| pathlossUmaNLOS \t| pathlossRmaLOS \t| pathlossRmaNLOS |\n")
+	for i := 0; i <= 20; i++ {
+		lng := 0 + (float64(i) * math.Pow(10, -3))
+
+		coord := model.Coordinate{Lat: 0.0001, Lng: lng}
+		dist3d := get3dEuclideanDistanceFromGPS(coord, cell)
+		urbanLOSPathLoss := getUrbanLOSPathLoss(coord, cell)
+		urbanNLOSPathLoss := getUrbanNLOSPathLoss(coord, cell)
+		ruralLOSPathLoss := getRuralLOSPathLoss(coord, cell)
+		ruralNLOSPathLoss := getRuralNLOSPathLoss(coord, cell)
+		fmt.Printf("%d \t| %.3v \t| %f \t| %f \t\t| %f \t\t| %f \t\t| %f \t|\n", i, coord, dist3d, urbanLOSPathLoss, urbanNLOSPathLoss, ruralLOSPathLoss, ruralNLOSPathLoss)
+
+		row := []string{
+			fmt.Sprintf("%d", i),
+			fmt.Sprintf("%.3v", coord),
+			fmt.Sprintf("%f", dist3d),
+			fmt.Sprintf("%f", urbanLOSPathLoss),
+			fmt.Sprintf("%f", urbanNLOSPathLoss),
+			fmt.Sprintf("%f", ruralLOSPathLoss),
+			fmt.Sprintf("%f", ruralNLOSPathLoss),
+		}
+		if err := writer.Write(row); err != nil {
+			t.Fatalf("failed to write row to CSV: %v", err)
+		}
 	}
 }

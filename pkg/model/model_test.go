282273323679cd18bf7f9488504c263f056f2a57
Konstantinos Kotsiaridis <konkots@intracom-telecom.com>
2025-03-28 10:12:50 +0200

    Fix TestModel unit test

--- pkg/model/model_test.go
+++ pkg/model/model_test.go
@@ -5,7 +5,6 @@
 package model
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/nfvri/onos-api/go/onos/ransim/types"
@@ -15,29 +14,19 @@ import (
 func TestModel(t *testing.T) {
 	model := &Model{}
 	err := LoadConfig(model, "test")
-	fmt.Printf("modell %+v", model)
 	assert.NoError(t, err)
 	t.Log(model)
 	assert.Equal(t, 0, len(model.Controllers))
 	assert.Equal(t, 3, len(model.Nodes))
 	assert.Equal(t, 3, len(model.Cells))
-	// assert.Equal(t, 0, model.Controllers["controller1"].Port)
-	// assert.Equal(t, 0, model.Controllers["controller2"].Port)
-	// assert.Equal(t, "1.0.0", model.ServiceModels["kpm"].Version)
-	// assert.Equal(t, 3, model.ServiceModels["rc"].ID)
-	// assert.Equal(t, 2, model.ServiceModels["ni"].ID)
 	assert.Equal(t, 0, len(model.ServiceModels))
 	assert.Equal(t, uint(12), model.UECount)
 	assert.Equal(t, "00101", model.Plmn)
 	assert.Equal(t, types.PlmnID(0x101), model.PlmnID)
 
-	assert.Equal(t, types.NCGI(0x10100000c003), model.Cells["managedelement=1193046,gnbdufunction=3,nrcelldu=3"].NCGI)
-	// TODO: Add cells in Nodes.Cells list
-	assert.Equal(t, 0, len(model.Nodes["managedelement=1193046"].Cells))
-	// fmt.Printf("ssssssss %+v", t)
-	fmt.Printf("new %+v", model.Cells["managedelement=1193048,gnbdufunction=1,nrcelldu=1"])
-	//assert.Equal(t, 37.981629, model.Cells["managedelement=1193048,gnbdufunction=1,nrcelldu=1"].Carriers[0].Center.Lat)
-	assert.Empty(t, model.Cells["managedelement=1193048,gnbdufunction=1,nrcelldu=1"].Carriers)
+	assert.Equal(t, types.NCGI(17680452386819), model.Cells["managedelement=1193046,gnbdufunction=3,nrcelldu=3"].NCGI)
+	assert.Equal(t, 1, len(model.Nodes["managedelement=1193046"].Cells))
+	assert.Equal(t, 37.981629, model.Cells["managedelement=1193048,gnbdufunction=1,nrcelldu=1"].Carriers[0].Center.Lat)
 
 	assert.Equal(t, false, model.MapLayout.FadeMap)
 	assert.Equal(t, 0.0, model.MapLayout.Center.Lat)
